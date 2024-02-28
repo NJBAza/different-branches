9e8d787da21563bbb414caec67146d2cec135263
NJBAza <njbuitragoa@gmail.com>
2024-02-28 02:59:17 -0500

    stuff

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Example Git Repository
 
 We are learning how to make commits.
+
+We are about to make a commit without best practices.
+
+1. make some changes
+2. git add -- "stage" the changes that we want to persist to our git history
+3. git commit -m "adding ..." ---creates a commit
--- git_tutorial.py
+++ git_tutorial.py
@@ -10,3 +10,15 @@ def add_number(a, b, c):
         _type_: _description_
     """    
     return a + b + c
+
+def calc_difference(a, b):
+    """_summary_
+
+    Args:
+        a (_type_): _description_
+        b (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """    
+    return a-b

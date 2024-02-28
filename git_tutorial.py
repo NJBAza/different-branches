475f44bc840f1e286469abf406acdecf5d9bdbec
NJBAza <njbuitragoa@gmail.com>
2024-02-28 01:49:43 -0500

    adding add_numbers function

---
+++ git_tutorial.py
@@ -0,0 +1,12 @@
+def add_number(a, b, c):
+    """_summary_
+
+    Args:
+        a (_type_): _description_
+        b (_type_): _description_
+        c (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """    
+    return a + b + c
